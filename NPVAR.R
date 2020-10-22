c56207544c34dd6b85fa1299974424dd70cae6e2
itsrainingdata <bryonsgit@gmail.com>
2020-10-22 11:05:46 -0500

    updates

--- NPVAR.R
+++ NPVAR.R
@@ -5,22 +5,22 @@ library("mgcv")
 #' @description Estimate topological ordering for nonparametric DAG with equal variance assumption
 #'
 #'
-#' @param x A n*p data matrix/dataframe w/o names
-#' @param run.np Estimate conditional variance using kernel regression
-#' @param run.mgcv Estimate conditional variance using GAM,
+#' @param x A n x p data matrix/dataframe w/o names
+#' @param run.np Use kernel regression to estimate conditional variance
+#' @param run.mgcv Use GAM to estimate conditional variance
 #'  If both are True, conduct both and output comparison for two methods
 #'  Use GAM for final result
-#' @param verbose Whether output detailed estimation process
-#' @param layer.select Whether do layer selection
-#' @param eta If layer.select is True, use this as fix eta for layer selection
+#' @param verbose Whether to output detailed estimation process
+#' @param layer.select Whether to do layer selection
+#' @param eta If layer.select is True, use this as fixed eta for layer selection
 #' @param x2 Same format as but independent with x. If layer.select is True and eta is NULL,
 #'  use x2 for estimation for eta adaptively
 #' @param eta.scaler A scaler to control the scale of eta when variance of the system is large.
 #'  The final eta = eta.scaler * eta_hat.
-#'  
-#'    
+#'
+#'
 #' @return If layer.select is True, return a list contained with a vector ancestors of ordering,
-#'  and a list of layers. If not, return a vector ancestors of ordering
+#'  and a list of layers. If not, return a vector of ancestors of ordering.
 NPVAR = function(x,
                  run.np = FALSE,
                  run.mgcv = TRUE,
@@ -33,7 +33,7 @@ NPVAR = function(x,
   if (layer.select & is.null(eta) & is.null(x2)) {
     stop("Dataset X2 shoud be provided if doing adaptive layer selection!")
   }
-  
+
   if(!is.data.frame(x)){
     x = data.frame(x)
   }
@@ -41,7 +41,7 @@ NPVAR = function(x,
   p = ncol(x)
   node.index = 1:p
   names(node.index) = names(x)
-  
+
   ### Find the first source by minimum marginal variance
   condvars = apply(x, 2, var)
   source = argmin(condvars)
@@ -62,11 +62,11 @@ NPVAR = function(x,
   } else {
     ancestors = c(source)
   }
-  
+
   if (verbose) {
     print(condvars)
   }
-  
+
   ### Main loop of algorithm
   l = 2
   while(length(ancestors) < p - 1){
@@ -90,16 +90,16 @@ NPVAR = function(x,
       }
       ancestors = c(ancestors, layers[[l]])
     } else {
-      ancestors = c(ancestors, source_next)  
+      ancestors = c(ancestors, source_next)
     }
-    
+
     l = l + 1 # layer indicator
-    
+
     if(verbose){
       print(condvars)
     }
   }
-  
+
   ### Last inclusion
   descendants = (node.index)[-ancestors]
   if (length(ancestors) < p) {
@@ -108,13 +108,13 @@ NPVAR = function(x,
       layers[[l]] = descendants
     }
   }
-  
+
   ### Final output
   print(names(ancestors))
   if (layer.select) {
     return(list(ancestors = ancestors, layers = layers))
   } else {
-    return(ancestors) 
+    return(ancestors)
   }
 }
 
@@ -122,18 +122,18 @@ NPVAR = function(x,
 ### A helper function to estimate conditional variance for descendants
 ### Parameters are the same in main function
 est.condvars = function(x, descendants, node.index, ancestors, verbose, run.np, run.mgcv) {
-  
+
   condvars = rep(NA, length(descendants))
   names(condvars) = names(descendants)
-  
+
   for(j in seq_along(descendants)){
-    
+
     current_node = descendants[j]
     if (verbose){
       message(sprintf("Checking %s ~ %s", toupper(names(node.index)[current_node]),
                       paste(toupper(names(ancestors)), collapse =" + ")))
     }
-    
+
     ### Compute nonparametric regression using kernel / np package
     if(run.np){
       npformula = "x[,current_node] ~ "
@@ -142,7 +142,7 @@ est.condvars = function(x, descendants, node.index, ancestors, verbose, run.np, 
       }
       npformula = substr(npformula, start = 1, stop = nchar(npformula) - 3)
       npformula = as.formula(npformula)
-      
+
       bw.all = npregbw(formula = npformula,
                        regtype = "ll",
                        bwmethod = "cv.aic",
@@ -154,7 +154,7 @@ est.condvars = function(x, descendants, node.index, ancestors, verbose, run.np, 
       )
       condvar.np = var(x[,current_node]) - var(fit.np)
     }
-    
+
     ### Compute nonparametric regression using GAM / mgcv package
     if(run.mgcv){
       mgcvformula = "x[,current_node] ~ "
@@ -163,18 +163,18 @@ est.condvars = function(x, descendants, node.index, ancestors, verbose, run.np, 
       }
       mgcvformula = substr(mgcvformula, start = 1, stop = nchar(mgcvformula) - 3)
       mgcvformula = as.formula(mgcvformula)
-      
+
       b1 = mgcv::gam(mgcvformula, data = x)
-      fit.gam = predict(b1) # BAD NEWS BEARS
-      
+      fit.gam = predict(b1)
+
       condvar.gam = var(x[,current_node]) - var(fit.gam)
     }
-    
+
     if(run.mgcv && run.np){
       message(sprintf("np estimate: %f / gam estimate: %f / difference: %f",
                       condvar.np, condvar.gam, condvar.np - condvar.gam))
     }
-    
+
     ### Record the estimated conditional variance
     if(run.np){
       condvars[j] = condvar.np
@@ -185,7 +185,7 @@ est.condvars = function(x, descendants, node.index, ancestors, verbose, run.np, 
     }
   }
   if(any(condvars < 0)) stop("Error: Negative variance found")
-  
+
   return(condvars)
 }
 
